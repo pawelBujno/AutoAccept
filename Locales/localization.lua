69990043fd81d6afd01e0d9f208589263a54a2c8
pawelBujno <34818983+pawelBujno@users.noreply.github.com>
2020-08-03 11:28:59 +0200

    Add files via upload

---
+++ Locales/localization.lua
@@ -0,0 +1,59 @@
+Global("Locales", {})
+
+function getLocale()
+	return Locales[common.GetLocalization()]
+end
+
+--------------------------------------------------------------------------------
+-- Russian
+--------------------------------------------------------------------------------
+
+Locales["rus"]={}
+Locales["rus"]["ReadyCheckStarted"]="Проверка готовности началась"
+Locales["rus"]["invitationp1"]="Вы приняли "
+Locales["rus"]["invitationp2"]=" приглашение от "
+Locales["rus"]["group"]="группа"
+Locales["rus"]["raid"]="набег"
+Locales["rus"]["closeMainButton"]="близко"
+Locales["rus"]["header1"]="AutoAccept Settings"
+Locales["rus"]["matchmakingLeaveOnEnd"]="auto Leave matchmaking After End"
+Locales["rus"]["autoInvite"]="auto Invite From Whisper (++)"
+Locales["rus"]["active"]="auto Accept Invite From Strangers"
+Locales["rus"]["acceptFriend"]="always accept Friend (++, --, lead)"
+Locales["rus"]["autoInviteFriend"]="auto Invite From Friend Whisper (++)"
+Locales["rus"]["autoLeave"]="auto Leave From Whisper (--)"
+Locales["rus"]["acceptGuild"]="always accept Guild (++, --, lead)"
+Locales["rus"]["autoPassLead"]="auto Pass Lead"
+Locales["rus"]["autoInviteGuildChat"]="auto invite from Guild Chat (++)"
+Locales["rus"]["autoAcceptQueue"]="auto accept Queue"
+Locales["rus"]["autoAcceptReadyCheck"]="auto accept Ready Check"
+Locales["rus"]["queueAsDD"]="queue as DD"
+Locales["rus"]["queueAsTank"]="queue as Tank"
+Locales["rus"]["queueAsHealer"]="queue as Healer"
+
+--------------------------------------------------------------------------------
+-- English
+--------------------------------------------------------------------------------
+
+Locales["eng_eu"]={}
+Locales["eng_eu"]["ReadyCheckStarted"]="Ready Check Started"
+Locales["eng_eu"]["invitationp1"]="You have accepted a "
+Locales["eng_eu"]["invitationp2"]=" invitation from "
+Locales["eng_eu"]["group"]="group"
+Locales["eng_eu"]["raid"]="raid"
+Locales["eng_eu"]["closeMainButton"]="Close"
+Locales["eng_eu"]["header1"]="AutoAccept Settings"
+Locales["eng_eu"]["matchmakingLeaveOnEnd"]="auto Leave matchmaking After End"
+Locales["eng_eu"]["autoInvite"]="auto Invite From Whisper (++)"
+Locales["eng_eu"]["active"]="auto Accept Invite From Strangers"
+Locales["eng_eu"]["acceptFriend"]="always accept Friend (++, --, lead)"
+Locales["eng_eu"]["autoInviteFriend"]="auto Invite From Friend Whisper (++)"
+Locales["eng_eu"]["autoLeave"]="auto Leave From Whisper (--)"
+Locales["eng_eu"]["acceptGuild"]="always accept Guild (++, --, lead)"
+Locales["eng_eu"]["autoPassLead"]="auto Pass Lead"
+Locales["eng_eu"]["autoInviteGuildChat"]="auto invite from Guild Chat (++)"
+Locales["eng_eu"]["autoAcceptQueue"]="auto accept Queue"
+Locales["eng_eu"]["autoAcceptReadyCheck"]="auto accept Ready Check"
+Locales["eng_eu"]["queueAsDD"]="queue as DD"
+Locales["eng_eu"]["queueAsTank"]="queue as Tank"
+Locales["eng_eu"]["queueAsHealer"]="queue as Healer"
